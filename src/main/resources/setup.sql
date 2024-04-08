5e89fb93d36d7f066e96229a43a2265a6d3d8b29
”Opanasenko-Mykhailo <opanasenko.misha@gmail.com>
2024-04-08 23:31:34 +0300

    update

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -9,3 +9,24 @@ CREATE TABLE `task` (
                         `status` int(11) NOT NULL,
                         PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+INSERT INTO `task` (`description`, `status`) VALUES
+                                                 ('Task 1', 1),
+                                                 ('Task 2', 0),
+                                                 ('Task 3', 1),
+                                                 ('Task 4', 0),
+                                                 ('Task 5', 1),
+                                                 ('Task 6', 0),
+                                                 ('Task 7', 1),
+                                                 ('Task 8', 0),
+                                                 ('Task 9', 1),
+                                                 ('Task 10', 0),
+                                                 ('Task 11', 1),
+                                                 ('Task 12', 0),
+                                                 ('Task 13', 1),
+                                                 ('Task 14', 0),
+                                                 ('Task 15', 1),
+                                                 ('Task 16', 0),
+                                                 ('Task 17', 1),
+                                                 ('Task 18', 0),
+                                                 ('Task 19', 1),
+                                                 ('Task 20', 0);
